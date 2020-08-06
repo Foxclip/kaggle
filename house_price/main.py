69bb5e97e8f6d9c58860e6ea8007f18dc3b4b14b
Foxclip <destroyApp2@gmail.com>
2020-08-06 23:23:51 +1000

    Prediction by two columns

--- house_price/main.py
+++ house_price/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import sys
 sys.path.insert(0, "e:\\Projects\\Python\\nn_sim\\")
 import nn_sim.dataset as dataset
@@ -7,55 +6,31 @@ import nn_sim.dataset as dataset
 if __name__ == "__main__":
 
     # Increasing number of columns so all of them are showed
-    pd.set_option('display.max_columns', 20)
+    pd.set_option('display.max_columns', 5)
 
     # loading datasets
+    train_df = pd.read_csv("train.csv")
     test_df = pd.read_csv("test.csv")
-    combined_df = pd.read_csv("train_age.csv")
+    combined_df = pd.concat([train_df, test_df])
+
+    target_col = "SalePrice"
 
     # transforming dataset
 
     df = combined_df.copy()
 
     # dropping unneeded columns
-    df = dataset.drop(df, ["Name", "PassengerId", "Ticket", "Cabin"])
+    # df = dataset.drop(df, ["Name", "PassengerId", "Ticket", "Cabin"])
+    df = df[["LotArea", "YearBuilt", "SalePrice"]]
 
     # filling missing values
-    df = dataset.impute(df, ["Fare", "Embarked"])
+    # df = dataset.impute(df, ["Fare", "Embarked"])
 
-    # whether passenger is alone
-    df["Family"] = df["SibSp"] + df["Parch"]
-    df["IsAlone"] = df["Family"] == 0
-    df = dataset.drop(df, ["SibSp", "Parch", "Family"])
-
-    # age categories
-    df["Age"] = pd.cut(
-        df["Age"],
-        (0, 18, 35, 60, 120),
-        labels=["Child", "Young", "Middle", "Old"]
-    )
-    # fare categories
-    df["Fare"] = pd.cut(
-        df["Fare"],
-        (0, 10, 100, 600),
-        include_lowest=True,
-        labels=["0-10", "10-100", "100-600"],
-    )
-
-    # # name prefixes
-    # def func(name):
-    #     parts = name.split(",")
-    #     prefix = parts[1].split(" ")[1]
-    #     return prefix
-    #
-    # df["Name"] = df["Name"].map(func)
-
-    df = dataset.label_encode(df, [])
-    # df = one_hot_encode(df, ["Sex", "Embarked", "Pclass"])
-    df = dataset.one_hot_encode(df, [
-        "Sex", "Embarked", "Pclass", "Age", "Fare"
-    ])
-    df = dataset.scale(df, exclude_cols=["Survived"])
+    # df = dataset.label_encode(df, [])
+    # df = dataset.one_hot_encode(df, [
+    #     "Sex", "Embarked", "Pclass", "Age", "Fare"
+    # ])
+    df = dataset.scale(df, exclude_cols=[target_col])
 
     # print(df)
     # import sys
@@ -63,28 +38,29 @@ if __name__ == "__main__":
 
     # specifying settings of a model
     model_settings = dataset.NeuralNetworkSettings()
-    model_settings.task_type = dataset.TaskTypes.binary_classification
+    model_settings.task_type = dataset.TaskTypes.regression
     model_settings.output_count = 1
     model_settings.epochs = 4
     model_settings.folds = 10
-    model_settings.target_col = "Survived"
-    model_settings.validation = dataset.ValidationTypes.none
+    model_settings.target_col = target_col
+    model_settings.validation = dataset.ValidationTypes.val_split
+    model_settings.checkpoint = False
+    model_settings.gpu = False
 
     # specifying lists of parameters
-    layers_lst = [5]
-    neurons_lst = [512]
-    # layers_lst = [2, 3, 4, 5]
-    # neurons_lst = [16, 32, 64, 128, 256, 512]
+    # layers_lst = [1]
+    # neurons_lst = [3]
+    layers_lst = [2, 3, 4, 5]
+    neurons_lst = [16, 32, 64, 128, 256, 512]
 
     # loading and preparing data
-    X_train, X_test = dataset.cut_dataset(df, target_col="Survived")
+    X_train, X_test = dataset.cut_dataset(df, target_col=target_col)
 
     # training models and saving file with predictions on test dataset
     dataset.train_models(X_train, model_settings, layers_lst, neurons_lst)
 
     # making predictions with the best model
-    predict = np.round(dataset.make_predictions(X_test))
-    final_df = pd.DataFrame(test_df["PassengerId"])
-    final_df["Survived"] = predict
-    final_df["Survived"] = final_df["Survived"].astype(int)
+    predict = dataset.make_predictions(X_test)
+    final_df = pd.DataFrame(test_df["Id"])
+    final_df[target_col] = predict
     final_df.to_csv("output.csv", index=False)
